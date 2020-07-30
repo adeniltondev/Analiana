0129c29a17223fc87aac637396a247fb566b9569
adeniltondev <adeniltonsilva1@hotmail.com>
2020-07-30 10:11:01 -0300

    rrr

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -166,3 +166,18 @@ window.onload = function() {
 			
 		})
 }
+
+plitting();
+$('.signup').submit(function(event){
+ event.preventDefault();
+ 
+ var emailText = $('.email').val();
+ $(".field").append("<p data-splitting='chars'>" + emailText + "</p>");
+  Splitting();
+  $("#wrap").addClass("plunge");
+  setTimeout(function(){	
+    $('.email').val('');
+    $( ".field p" ).remove();
+    $("#wrap").removeClass("plunge");
+}, 4000);
+});
