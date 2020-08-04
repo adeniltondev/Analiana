627961feafc2b8d5803344611231f2c718f326a2
adeniltondev <adeniltonsilva1@hotmail.com>
2020-08-04 10:34:33 -0300

    ddd

--- assets/js/customizacao.js
+++ assets/js/customizacao.js
@@ -162,3 +162,13 @@ function toggleDropdown (e) {
 $('body')
   .on('mouseenter mouseleave','.dropdown',toggleDropdown)
   .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+var $slider = document.getElementById('slider');
+var $toggle = document.getElementById('toggle');
+
+$toggle.addEventListener('click', function() {
+    var isOpen = $slider.classList.contains('slide-in');
+
+    $slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
+});
